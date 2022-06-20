0095a6b12432c3d991baa3fbead153117b9bac66
jansen her <jansen.her@foodpanda.com>
2022-06-20 10:29:51 +0800

    Add zsh alias and functions

---
+++ alias.zsh
@@ -0,0 +1,4 @@
+alias subl="sublime"
+alias pablo="cd ~/Documents/panda/pd-pablo-payment-gateway"
+alias vim="nvim"
+
---
+++ gitfunctions.zsh
@@ -0,0 +1,11 @@
+unalias gcl
+gcl() {
+	git checkout $(git branch | fzf)
+}
+gcr() {
+	  local branches branch
+	  branches=$(git branch --all | grep -v HEAD) &&
+	  branch=$(echo "$branches" |
+		   fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+	  git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+}
