89e873d9f9758ed48a2fb461bfaa0c1eb9c2055b
jansen her <jansen.her@foodpanda.com>
2022-07-21 22:24:14 +0800

    Add some handy aliases.

--- alias.zsh
+++ alias.zsh
@@ -2,3 +2,8 @@
 alias vim="nvim"
 alias ls="exa"
 
+# alias just for panda.
+alias migratetest="docker exec -it testing_pablo_method_worker_1 make migrate"
+alias removetest="docker-compose -f deployments/testing/docker-compose.yaml down --remove-orphans"
+alias stoptest="docker-compose -f deployments/testing/docker-compose.yaml stop"
+
